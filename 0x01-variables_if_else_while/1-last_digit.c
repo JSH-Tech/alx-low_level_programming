db2ff3186d9dee981d0f4f0a3ddbca8d4da0d92b
Josue Abotsidia <josueabots@gmail.com>
2022-07-01 10:54:44 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,15 +19,15 @@ n = rand() - RAND_MAX / 2;
 ld = (n % 10);
 if ((ld < 6) && (ld != 0))
 {
-printf("Last digit of ", $n " is", $ld " and is less than 6 and not 0")
+printf("Last digit of ", $n " is", $ld " and is less than 6 and not 0");
 }
 else if ((ld > 5))
 {
-printf("Last digit of ", $n " is", $ld " and is greater than 5")
+printf("Last digit of ", $n " is", $ld " and is greater than 5");
 }
 else if ((ld == 0))
 {
-printf("Last digit of ", $n "is", $ld "and 0")
+printf("Last digit of ", $n "is", $ld "and 0");
 }
 
 return (0);
