da1b295567d5270634f5d28b43e6b5f304def245
Josue Abotsidia <josueabots@gmail.com>
2022-07-08 09:23:01 -0700

    task 1 isidigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+int _isdigit(int c)
+{
+/**
+ * isdigit - function that checks for digit
+ *@c is number to enter
+ * Return: 1 or 0.
+ */
+}
+
+int _isdigit(int c)
+{
+if(c >= 0 && c <= 9)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
