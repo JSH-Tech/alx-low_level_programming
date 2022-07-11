925f080c79be50a47752bdd9b39c676dc81cef88
Josue Abotsidia <josueabots@gmail.com>
2022-07-11 13:59:33 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ *reset_to_98 - function taht reset the value of a number
+ *
+ *@n : is a parameter
+ */
 void reset_to_98(int *n)
 {
-*n=98;
+*n = 98;
 }
