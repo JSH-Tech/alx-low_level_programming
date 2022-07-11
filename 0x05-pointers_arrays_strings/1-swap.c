7db3fcd4d58e4fda108a36d2a4951a6e7e51bf3f
Josue Abotsidia <josueabots@gmail.com>
2022-07-11 15:08:53 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *swap_int - function that swap values of two integers
+ *
+ *@a : is a parameter
+ *
+ *@b : is a parameter
+ */
+
+void swap_int(int *a, int *b)
+{
+int *val;
+int *val2;
+val = &a;
+val2 = &b;
+*val = b;
+*val2 = a;
+}
