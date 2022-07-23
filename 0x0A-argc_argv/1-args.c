1fb90dec7d9aa24c0b5f613fe21cf48793ab5141
Josue Abotsidia <josueabots@gmail.com>
2022-07-23 07:27:56 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *main - is function entry
+ *
+ *@argc : a parameter
+ *@argv : a parameter type char
+ *return (0) : always success
+ */
+
+int main(int argc, char *argv[])
+{
+(void)argc;
+ printf("%d\n", (argc - 1));
+
+return (0);
+}
