141510886e4e8a02a68a11b4826be0317177601e
Josue Abotsidia <josueabots@gmail.com>
2022-07-23 06:26:01 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - is function entry
+ *
+ *@argc : a parameter
+ *@argv : a parameter type char
+ *return (0) : always success
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+
+return (0);
+}
