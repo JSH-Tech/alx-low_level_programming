1b7d89a8ee027f36a273cf9fced6601aadadf6f8
Josue Abotsidia <josueabots@gmail.com>
2022-08-02 11:58:42 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,10 +15,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *newdog;
 newdog = malloc(sizeof(dog_t));
+if (newdog==NULL)
+return (NULL);
 strcpy(newdog->name, name);
 newdog->age = age;
 strcpy(newdog->owner, owner);
 return (newdog);
-if (newdog == NULL)
-return (NULL);
 }
