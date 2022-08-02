755705a9963163e8632727d85b134f5c15662594
Josue Abotsidia <josueabots@gmail.com>
2022-08-02 04:32:11 -0700

    Header files

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - describes the sweet boy or girl
+ *
+ *@name:dog's name
+ *@age:The dog's age
+ *@owner: The owner of the dog
+ */
+
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+
+
+#endif
